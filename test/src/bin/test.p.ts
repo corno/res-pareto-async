997093a12dc0a67e41585084513f4730e6bc626a
corno <corno.astn.a@schraverus.com>
2022-09-25 09:12:24 +0200

    .

--- test/src/bin/test.p.ts
+++ test/src/bin/test.p.ts
@@ -1,7 +1,23 @@
+#!/usr/bin/env node
 
-import * as pl from "pareto-core-lib"
+import * as pe from "pareto-core-exe"
 
-import * as lib from "../../../pub/dist"
-pl.logDebugMessage("NO TESTS YET")
+import * as test from "lib-pareto-test"
 
-//lib.createDictionaryMapper
+import { createGetTestset } from "../implementation"
+import { dependencies } from "../dependencies/dependencies.p"
+import { data } from "../data/data.p"
+
+
+pe.runProgram(
+    test.f_createTester(
+        null,
+        {
+            getTestSet: createGetTestset(
+                data,
+                dependencies
+            ),
+            dependencies: test.dependencies,
+        },
+    )
+)
---
+++ test/src/interface/functions/functions.p.ts
@@ -0,0 +1,7 @@
+import * as test from "lib-pareto-test"
+import { DDependencies } from "../dependencies/dependencies.p"
+
+export type FCreateGetTestset = (
+    $: {},
+    $d: DDependencies
+) => test.FGetTestSet
